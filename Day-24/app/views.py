daac2169d894a9203e3b1196a4a502451f5aea36
Arun Sah <saharun2056@gmail.com>
2026-02-16 22:57:36 +0530

    learn to store the client message and able to send automatic email

--- Day-24/app/views.py
+++ Day-24/app/views.py
@@ -5,14 +5,21 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from .forms import ContactForm
 from django.conf import settings
-
+from .models import Contact
 def contact_view(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             user_email = form.cleaned_data["email"]
             name = form.cleaned_data["name"]
+            message = form.cleaned_data["message"]
+            # Save the contact message to the database
 
+            Contact.objects.create(
+                name=name,
+                email=user_email, 
+                message=message
+                )
             # Send auto greeting email
             send_mail(
                 subject="Thanks for contacting us!",
